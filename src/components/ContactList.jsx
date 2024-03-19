9481b0ea865bc9319bdae559c41b142c866ee23b
PYLYP CHELBARAKH <143128807+mrignis@users.noreply.github.com>
2024-03-19 16:07:03 -0400

    up

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faPhone } from '@fortawesome/free-solid-svg-icons'; // Імпортуємо необхідні іконки
 import styles from './ContactList.module.css'; // Імпортуємо CSS-модуль
-
 
 function ContactList({ contacts, onDeleteContact }) {
   return (
-    <ul className={styles.contactList}> {/* Додаємо клас з CSS-модуля */}
+    <ul className={styles.contactList}>
       {contacts.map(contact => (
-        <li key={contact.id} className={styles.contactItem}> {/* Додаємо клас з CSS-модуля */}
+        <li key={contact.id} className={styles.contactItem}>
           <div>
-            <span className={styles.contactName}>{contact.name}:</span>
+            <span className={styles.contactName}>
+            <FontAwesomeIcon icon={faUser} /> {contact.name}
+            </span>
           </div>
           <div>
-            <span className={styles.contactNumber}>{contact.number}</span>
+            <span className={styles.contactNumber}>
+              <FontAwesomeIcon icon={faPhone} /> {contact.number}
+            </span>
           </div>
           <button className={styles.deleteButton} onClick={() => onDeleteContact(contact.id)}>Delete</button>
         </li>
@@ -21,4 +26,3 @@ function ContactList({ contacts, onDeleteContact }) {
 }
 
 export default ContactList;
-
